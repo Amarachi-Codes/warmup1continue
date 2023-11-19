06de137a9a8ecf2d8d4728daaf74ed9460bee31c
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-19 22:46:42 +0100

    startoz

--- startoz.js
+++ startoz.js
@@ -1,11 +1,11 @@
-// Warmup-1 -- startOz
-// Given a string, return a string made of the first 2 chars (if present), however include first char only if it is 'o' and include the second only if it is 'z', so 'ozymandias' yields 'oz'.
+Warmup-1 -- startOz
+Given a string, return a string made of the first 2 chars (if present), however include first char only if it is 'o' and include the second only if it is 'z', so 'ozymandias' yields 'oz'.
 
-// Examples
+Examples
 
-// startOz('ozymandias') → oz
-// startOz('bzoo') → z
-// startOz('oxx') → o
+startOz('ozymandias') → oz
+startOz('bzoo') → z
+startOz('oxx') → o
 
 function startOz(str){
     if(str[0]==="o" && str[1] ==="z"){
