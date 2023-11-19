282a5d567feb1aeb486ac7f4d914b89836757187
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-19 21:53:52 +0100

    backaround

--- backaround.js
+++ backaround.js
@@ -1,8 +1,13 @@
-Warmup-1 -- backAround
-Given a string, take the last char and return a new string with the last char added at the front and back, so 'cat' yields 'tcatt' The original string will be length 1 or more.
+// Warmup-1 -- backAround
+// Given a string, take the last char and return a new string with the last char added at the front and back, so 'cat' yields 'tcatt' The original string will be length 1 or more.
 
-Examples
+// Examples
 
-backAround('cat') → tcatt
-backAround('Hello') → oHelloo
-backAround('a') → aaa
+// backAround('cat') → tcatt
+// backAround('Hello') → oHelloo
+// backAround('a') → aaa
+
+function backAround(str){
+    let last = str[str.length-1]
+    return last + str + last 
+  }console.log(backAround('cat'));
