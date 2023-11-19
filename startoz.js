9c916ef504fc7d5736455385aa76c54042313650
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-19 22:46:06 +0100

    startoz

---
+++ startoz.js
@@ -0,0 +1,18 @@
+// Warmup-1 -- startOz
+// Given a string, return a string made of the first 2 chars (if present), however include first char only if it is 'o' and include the second only if it is 'z', so 'ozymandias' yields 'oz'.
+
+// Examples
+
+// startOz('ozymandias') → oz
+// startOz('bzoo') → z
+// startOz('oxx') → o
+
+function startOz(str){
+    if(str[0]==="o" && str[1] ==="z"){
+    return "oz"
+    }if(str[0] === "o"){
+    return "o"
+    }if(str[1] === "z"){
+    return "z"
+    }return ""
+  }console.log(startOz('ozymandias'));
