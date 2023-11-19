24de9de6fbfdc8fa7298acde0064303391ec5bb5
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-19 21:47:46 +0100

    updated

---
+++ frontback.js
@@ -0,0 +1,14 @@
+// Warmup-1 -- frontBack
+// Given a string, return a new string where the first and last chars have been exchanged.
+
+// Examples
+
+// frontBack('code') → eodc
+// frontBack('a') → a
+// frontBack('ab') → ba
+
+function frontBack(str){
+    let front = str[0]
+    let back = str[str.length-1]
+  return back + str.slice(1, str.length-1)+ front
+}console.log(frontBack('code'));
